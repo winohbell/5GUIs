c68d0c756cffe89e52485107231ef29610f2cfab
Tanzhi Liu <winohbell@yahoo.com>
2021-05-19 23:13:49 -0400

    finish timer

--- client/src/Timer.jsx
+++ client/src/Timer.jsx
@@ -3,13 +3,53 @@ import React from 'react';
 export default class Timer extends React.Component {
     constructor(props) {
       super(props);
-      this.state = {}
+      this.state = {
+        duration: 10,
+        elapsed: 0
+      }
+
+      this.timer = this.timer.bind(this);
+    }
+
+    handledurationChange(e) {
+        this.setState({duration: Number(e.target.value)})
+    }
+
+    resetTimer() {
+        this.setState({elapsed: 0})
+    }
+
+
+    //timer
+    timer() {
+        let newElapsed = this.state.elapsed + 0.1;
+        if(newElapsed <= this.state.duration) {
+            this.setState({elapsed: newElapsed})
+        }
+    }
+    componentDidMount() {
+        this.interval = setInterval(this.timer, 100)
+    };
+
+    componentWillUnmount() {
+        clearInterval(this.interval)
     }
 
     render() {
       return (
         <div id="Timer">
-            Here's Timer
+            <div>
+                Elapsed time: 
+                <progress max={this.state.duration} value={this.state.elapsed}/>
+            </div>
+            <div>
+                <span>{this.state.elapsed.toFixed(1)}s</span>
+            </div>
+            <div>
+                Duration:
+                <input type="range" min={0} max={15} value={this.state.duration} onChange={e => {this.handledurationChange(e)}} />
+            </div>
+            <button onClick={e => {this.resetTimer(e)}}>Reset</button>
         </div>
       )
     }
